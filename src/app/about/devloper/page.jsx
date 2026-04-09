f21f301b8108d524b4559cdbc05b76b7ace71f25
almahmudzihad <amzihad@gmail.com>
2026-04-09 14:25:09 +0600

    first commit

---
+++ src/app/about/devloper/page.jsx
@@ -0,0 +1,11 @@
+
+
+function page() {
+    return (
+        <div>
+            <h1 className="text-7xl text-red-400">This is about devloper page component</h1>
+        </div>
+    )
+}
+
+export default page
